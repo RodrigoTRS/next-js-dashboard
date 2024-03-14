1b559fb9e0f5b5adcb57a38dbac362924f15ebb9
Rodrigo Teixeira Ribeiro da Silva <rodrigoteix1998@gmail.com>
2024-03-14 17:15:45 -0300

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const primaryFont = Inter({ subsets: ['latin'] });
+export const secondaryFont = Lusitana({ subsets: ['latin'], weight: ["400", "700"]})
